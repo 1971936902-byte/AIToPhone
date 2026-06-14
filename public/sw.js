12a68f25378eb17a644a6f1400057a1d3cf5a30e
1971936902-byte <1971936902@qq.com>
2026-06-14 14:11:28 +0800

    Refine project list disclosure styling

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
-const CACHE = "aitophone-v9";
-const ASSETS = ["/", "/index.html", "/styles.css?v=9", "/app.js?v=9", "/manifest.webmanifest", "/icon.svg"];
+const CACHE = "aitophone-v10";
+const ASSETS = ["/", "/index.html", "/styles.css?v=10", "/app.js?v=10", "/manifest.webmanifest", "/icon.svg"];
 
 self.addEventListener("install", (event) => {
   event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(ASSETS)));
